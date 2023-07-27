fae131bec88bccb3c368dccdffdf3ef32a47ecfc
Andriana <andrianasaffo@gmail.com>
2023-07-27 19:34:58 +0100

    Create folder for clickable cards
    Folder containing js and css file. Component is for team card that shows the team name.
    Css applied to js component

---
+++ client/src/components/ClickableCards/ClickableCards.js
@@ -0,0 +1,11 @@
+import "../ClickableCards/ClickableCards.css";
+
+const ClickableCards = () => {
+  return (
+    <main>
+      <div className="team-box">Dummy Team</div>
+    </main>
+  );
+};
+
+export default ClickableCards;

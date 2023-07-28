deda9d095ba6a7fa9550e9ab8cdcdd986eca6aaf
Andriana <andrianasaffo@gmail.com>
2023-07-28 08:32:14 +0100

    Create CardInfo.js
    Create component, information hardcoded until backend code is complete

---
+++ client/src/components/CardInfo/CardInfo.js
@@ -0,0 +1,16 @@
+import "../CardInfo/CardInfo.css";
+
+const CardInfo = () => {
+  return (
+    <div>
+      <h3>Team members</h3>
+      <aside>
+        <span className="color-box"></span>
+        <span className="name">John Smith</span>
+        <span className="percentage">30%</span>
+      </aside>
+    </div>
+  );
+};
+
+export default CardInfo;

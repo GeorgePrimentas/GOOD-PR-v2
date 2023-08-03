25e1c141c566eae39932bd6a26d932f54c3ce6cd
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-03 20:52:18 +0100

    download slack icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
+import GitHubIconSvg from "./components/Icons/GitHubIcon";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
+      <GitHubIconSvg />
     </div>
   );
 }

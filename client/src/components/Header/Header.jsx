5c841d362e21fb85b3b8764b5de3d9549b5052cb
George <108883931+GeorgePrimentas@users.noreply.github.com>
2023-08-03 12:47:50 +0100

    Import NavBar component into Header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import logo from '../../images/cyf-logo.png';
+import NavBar from '../NavBar/NavBar.jsx';
 import './Header.css';
 
 const Header = ({ title }) => {
-
 return (
     <header className="App-header">
       <div className="logo-container">
         <img className="logo" src={logo} alt="CYF logo" />
       </div>
+      <NavBar />
     </header>
 )
-
 }
 
 export default Header;

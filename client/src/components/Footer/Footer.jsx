fd215d8fcdf48aa401aabee53a6e70ba6b036ca5
George <108883931+GeorgePrimentas@users.noreply.github.com>
2023-08-03 19:18:58 +0100

    Skeleton of the Footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = ({ title }) => {
+
+return (
+    <footer className="App-footer">
+      <div className="">
+        <p>Made with love by Team Alpha in the Summer of 2023</p>
+      </div>
+    </footer>
+)
+
+}
+
+export default Footer;

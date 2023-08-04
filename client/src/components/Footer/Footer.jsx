bb50b93dabf27ce03edd4b09edc7d59a80c63126
George <108883931+GeorgePrimentas@users.noreply.github.com>
2023-08-04 06:59:22 +0100

    Update Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,9 +5,7 @@ const Footer = ({ title }) => {
 
 return (
     <footer className="App-footer">
-      <div>
         <p>Made with love by <a className="footer-link" href="#">Team Alpha</a> in the Summer of 2023</p>
-      </div>
     </footer>
 )
 

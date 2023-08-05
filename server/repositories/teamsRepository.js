907eddf776ea36ddd859cfa380afd018d01e4da0
Andriana <andrianasaffo@gmail.com>
2023-08-05 15:07:04 +0100

    Update teamsRepository.js
    Database to return all information

--- server/repositories/teamsRepository.js
+++ server/repositories/teamsRepository.js
@@ -1,16 +1,14 @@
 import { dataBase } from "../index.js";
 
-export async function getAllTeamNames() {
+export async function getAllTeamInfo() {
   try {
-    const result = await dataBase.query("SELECT team_name FROM fp_teams");
+    const result = await dataBase.query("SELECT * FROM fp_teams");
 
     if (result.rowCount === 0) {
       console.log("No teams available");
       throw new Error("No teams available");
     } else {
-      return result.rows.map((eachTeamName) => {
-        return eachTeamName.team_name;
-      });
+      return result.rows;
     }
   } catch (error) {
     console.error("Fetching teams:", error);

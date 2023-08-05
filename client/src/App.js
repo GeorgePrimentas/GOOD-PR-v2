8315ca825c55f50a3f69cf67bdbe24d097821a79
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-05 14:11:08 +0100

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
-// import GitHubIconSvg from "./components/Icons/GitHubIcon";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
-      {/* <GitHubIconSvg /> */}
+      
     </div>
   );
 }

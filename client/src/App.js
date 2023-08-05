3fe0bbb1c150a726619b0792974f769054a5efcf
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-05 13:00:10 +0100

    conflict resolve

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
-import GitHubIconSvg from "./components/Icons/GitHubIcon";
+// import GitHubIconSvg from "./components/Icons/GitHubIcon";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
-      <GitHubIconSvg />
+      {/* <GitHubIconSvg /> */}
     </div>
   );
 }

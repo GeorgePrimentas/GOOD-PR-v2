f93dfb4c40a8c5a5b1411a5bdf87015feee70de5
Andriana <andrianasaffo@gmail.com>
2023-08-05 15:07:42 +0100

    Update teamsController.js
    function name changed

--- server/controllers/teamsController.js
+++ server/controllers/teamsController.js
@@ -1,9 +1,8 @@
-import { getAllTeamNames } from "../repositories/teamsRepository.js";
+import { getAllTeamInfo } from "../repositories/teamsRepository.js";
 
 export async function getTeamData(req, res) {
   try {
-    const allTeamNames = await getAllTeamNames();
-    console.log(allTeamNames);
+    const allTeamNames = await getAllTeamInfo();
     return res.status(200).json(allTeamNames);
   } catch (error) {
     console.error("Error fetching team data:", error);

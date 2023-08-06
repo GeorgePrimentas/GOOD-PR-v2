80894958d29837426391b70c3c63ce0592125f31
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-07 00:41:20 +0100

    made register teams button to nav

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
 import './NavBar.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Form from "../Form/Form";
 
 const NavBar = ({ title }) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [showMenu, setShowMenu] = useState(false); // State for hamburger menu visibility
   const [showAbout, setShowAbout] = useState(false); // State for showing the About component
+  const [showForm, setShowForm] = useState (false); //(To hide Form upon page load)
+
+  // Update windowWidth state on window resize
+  // useEffect(() => {
+  //   const handleResize = () => setWindowWidth(window.innerWidth);
+  //   window.addEventListener("resize", handleResize);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
 
   // Update windowWidth state on window resize
   useEffect(() => {
@@ -18,6 +27,11 @@ const NavBar = ({ title }) => {
   const handleHamburgerClick = () => {
     setShowMenu(!showMenu);
   };
+
+  //Toggle Form visibility upon clicking Team regitration nav bar element
+  const handleTeamRegistrationFormClick = () => {
+    setShowForm(!showForm);
+  }
 
   // Add event listener for hamburger menu visibility on window resize
   useEffect(() => {
@@ -49,12 +63,15 @@ const NavBar = ({ title }) => {
             <Link to="/">
               Home
             </Link>
-            <Link to="/form">
+            {/* <Link to="/form">
               Team Registration
-            </Link>
+            </Link> */}
             <Link to="/search">
               Search
             </Link>
+              <button className="link-button" onClick={handleTeamRegistrationFormClick}>
+            Team Registration
+          </button>
           </nav>
         )}
         {showMenu && (
@@ -62,14 +79,20 @@ const NavBar = ({ title }) => {
             <Link to="/" className="list-links">
               Home
             </Link>
-            <Link to="/form" className="list-links">
-              Team Registration
-            </Link>
+             <button className="list-links" onClick={handleTeamRegistrationFormClick}>
+            Team Registration
+          </button>
             <Link to="/search" className="list-links">
               Search
             </Link>
           </div>
         )}
+
+         {showForm && (
+        <div className="team-registration-form">
+          <Form/>
+        </div>
+      )}
       </div>
      
   );

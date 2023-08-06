60a2492b8079cc7a6194f777a3339661d8e50144
Andriana <andrianasaffo@gmail.com>
2023-08-06 12:40:32 +0100

    Update teamsController.js
    Return data gotten from database

--- server/controllers/teamsController.js
+++ server/controllers/teamsController.js
@@ -1,9 +1,20 @@
 import { getAllTeamInfo } from "../repositories/teamsRepository.js";
+import { getAllTeamAndMembersInfo } from "../repositories/teamsRepository.js";
 
 export async function getTeamData(req, res) {
   try {
     const allTeamNames = await getAllTeamInfo();
     return res.status(200).json(allTeamNames);
+  } catch (error) {
+    console.error("Error fetching team data:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+export async function getTeamAndMemberData(req, res) {
+  try {
+    const allTeamAndMemberData = await getAllTeamAndMembersInfo();
+    return res.status(200).json(allTeamAndMemberData);
   } catch (error) {
     console.error("Error fetching team data:", error);
     return res.status(500).json({ error: "Internal server error" });

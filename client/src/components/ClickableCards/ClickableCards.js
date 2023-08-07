baec0d9e8c79864b0a6f06672273bc1a82d17a7a
George <108883931+GeorgePrimentas@users.noreply.github.com>
2023-08-07 18:48:09 +0100

    Update ClickableCards.js - Name Length
    If a Team's name is longer than 20 characters, only the first 20 characters will be displayed in the button

--- client/src/components/ClickableCards/ClickableCards.js
+++ client/src/components/ClickableCards/ClickableCards.js
@@ -1,6 +1,13 @@
 import "../ClickableCards/ClickableCards.css";
 
 const ClickableCards = ({ teamName }) => {
+  // The following if statement will 'slice' the name of the 
+  // Team and keep only the first 20 characters (if it's longer).
+  // It will also add an ellipsis ("...") to convey that the name
+  // displayed has been sliced.
+  if (teamName.length > 20) {
+    teamName = teamName.substring(0, 20) + "...";
+  }
   return (
     <main>
       <div className="team-box">{teamName}</div>

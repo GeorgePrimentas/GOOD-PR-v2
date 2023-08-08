f61b6082a4fc2810d704903e31620de1d60cce16
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-08 02:25:11 +0100

    creating google Icon for pr linking brief

---
+++ client/src/components/Icons/GoogleDocIcon.jsx
@@ -0,0 +1,17 @@
+import GoogleDocIcon from "../components/GoogleDocsLogo.svg";
+
+
+
+
+const GoogleDocsLogosvg = () => {
+    return (
+       < a 
+       className="google-doc-project-brief-icon" 
+        href="https://github.com/Gayle-Thompson-Igwebike/GOOD-PR-v1">
+       <img src={GoogleDocIcon} alt="github icon"  /> 
+       </a>
+    )
+
+}
+
+export default GoogleDocsLogosvg;

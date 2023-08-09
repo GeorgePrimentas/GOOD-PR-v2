00f4797ef44f99e662c3a0c5b7ad0c0b82794af4
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-09 23:19:39 +0100

    classname update

--- client/src/components/Icons/ProjectBoardIcon.jsx
+++ client/src/components/Icons/ProjectBoardIcon.jsx
@@ -6,7 +6,7 @@ import ProjectBoardIcon from "../../images/github-project-icon.svg";
 const ProjectBoardIconSvg = () => {
     return (
        < a 
-       className="projectBoard-icon" 
+       className="project-board-icon" 
         href="https://github.com/Gayle-Thompson-Igwebike/GOOD-PR-v1">
        <img src={ProjectBoardIcon} alt="github icon"  /> 
        </a>

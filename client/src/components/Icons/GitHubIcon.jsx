86c78cb3f746f9782224401f892af80df8549548
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-10 11:24:52 +0100

    app js update

--- client/src/components/Icons/GitHubIcon.jsx
+++ client/src/components/Icons/GitHubIcon.jsx
@@ -1,18 +1,31 @@
+import React from "react";
 import GitHubIcon from "../../images/github-mark.svg";
+import "./GitHubIcon.css"
 
 
 
 
 const GitHubIconSvg = () => {
+
     return (
+        <div className="icons-container">
        < a 
        className="githubrepo-icon" 
         href="https://github.com/Gayle-Thompson-Igwebike/GOOD-PR-v1">
        <img src={GitHubIcon} alt="github icon"  /> 
        </a>
+       </div>
     )
 
 }
 
 export default GitHubIconSvg;
+
+
+
+
+
+
+
+
 
--- client/src/components/Icons/ProjectBoardIcon.jsx
+++ client/src/components/Icons/ProjectBoardIcon.jsx
@@ -1,15 +1,18 @@
 import ProjectBoardIcon from "../../images/github-project-icon.svg";
+import "./ProjectBoardIcon.css"
 
 
 
 
 const ProjectBoardIconSvg = () => {
     return (
+        <div className="icons-container">
        <a 
        className="project-board-icon" 
         href="https://github.com/Gayle-Thompson-Igwebike/GOOD-PR-v1">
        <img src={ProjectBoardIcon} alt="github icon"  /> 
        </a>
+       </div>
     )
 
 }

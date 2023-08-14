ebbd58f721117de10959e0bf9245867de9d9d4ad
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-14 21:31:15 +0100

    slaxk update

--- client/src/components/Icons/SlackIconSvg.jsx
+++ client/src/components/Icons/SlackIconSvg.jsx
@@ -8,8 +8,9 @@ const SlackIconSvg = () => {
        < a 
        className="slack-icon" 
         href="https://github.com/Gayle-Thompson-Igwebike/GOOD-PR-v1">
-       <img src={SlackIcon} alt="slack icon"/> <style> height: 200px</style>
+       <img src={SlackIcon} alt="slack icon"/>
        </a>
     )
 
 }
+export default SlackIconSvg;

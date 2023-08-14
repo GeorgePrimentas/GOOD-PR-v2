f09275077b6e1e198e00eb32bd7b2d28e95d2804
Gayle Ngozi <gaylengozi@gmail.com>
2023-08-14 21:30:51 +0100

    update slackicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,74 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
 import ClickableCards from "./components/ClickableCards/ClickableCards";
-import CardInfo from "./components/CardInfo/CardInfo";
 
+// import CardInfo from "./components/CardInfo/CardInfo";
+import Search from "./components/Search/Search";
+import TrafficLights from "./components/TrafficLights/TrafficLights";
 
 
 
 
 function App() {
   const [teamAndMemberData, setTeamAndMemberData] = useState([]);
+  const [teamData, setTeamData] = useState([]);
+  const [teamStatuses, setTeamStatuses] = useState([]); // Define the teamStatuses state
 
-  function getAllTeamsAndMembersData() {
-    fetch("https://good-pr-v1-server.onrender.com/api/members")
+  async function getAllTeamsAndMembersData() {
+    try {
+      const response = await fetch("https://good-pr-v1-server.onrender.com/teamPr"); //Change to render site before merging
+      const data = await response.json();
+      setTeamAndMemberData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  function getAllTeamData() {
+    fetch("https://good-pr-v1-server.onrender.com/team")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setTeamAndMemberData(data);
+        setTeamData(data);
       })
       .catch((error) => console.log(error));
   }
 
   useEffect(() => {
     getAllTeamsAndMembersData();
+    getAllTeamData();
   }, []);
 
   return (
     <div className="App">
       <Header />
+      <Search />
       <section className="team-buttons">
-        {teamAndMemberData.length > 0 &&
-          teamAndMemberData.map((eachTeam) => (
-            <ClickableCards key={eachTeam.id} teamName={eachTeam.teamName} />
+        {/* {teamAndMemberData.length > 0 &&
+          teamAndMemberData.map((eachInfo) => (
+            <CardInfo
+              key={eachInfo.id}
+              pr={eachInfo.pullRequestCount}
+              allUsers={eachInfo.users}
+            />
+          ))} */}
+
+        {teamData.length > 0 &&
+          teamData.map((eachTeam) => (
+            <ClickableCards
+              key={eachTeam.id}
+              teamName={eachTeam.team_name}
+              teamId={eachTeam.id}
+              teamStatuses={teamStatuses} // Pass teamStatuses to ClickableCards
+            />
           ))}
       </section>
-
-      {teamAndMemberData.length > 0 &&
-        teamAndMemberData.map((eachInfo) => (
-          <CardInfo
-            key={eachInfo.id}
-            pr={eachInfo.pullRequestCount}
-            allUsers={eachInfo.users}
-          />
-        ))}
-
+      <TrafficLights
+        teams={teamAndMemberData}
+        setTeamStatuses={setTeamStatuses}
+      />{" "}
+      {/* Pass setTeamStatuses to TrafficLights */}
       <Form />
      
       

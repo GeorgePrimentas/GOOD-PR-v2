737f25e21b8ec9388ebf20f072d7efb5639881a7
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2023-08-14 22:33:07 +0100

    commented out the router in Header and footer
    to be able to have all the Routers in one place in app.js

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import './Footer.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Footer = ({ title }) => {
 
 return (
     <footer className="App-footer">
-        <p>Made with love by <Link className="footer-link" to="/about">Team Alpha</Link> in the Summer of 2023</p>
+        {/* <p>Made with love by <Link className="footer-link" to="/about">Team Alpha</Link> in the Summer of 2023</p> */}
     </footer>
 )
 

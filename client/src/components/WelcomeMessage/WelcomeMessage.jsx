81c611d1c2d25b31f9fc7acbe152beea2628088f
Andriana <andrianasaffo@gmail.com>
2023-08-16 10:11:55 +0100

    Create WelcomeMessage.jsx
    Welcome message component created and information about what app does included

---
+++ client/src/components/WelcomeMessage/WelcomeMessage.jsx
@@ -0,0 +1,14 @@
+function WelcomeMessage() {
+  return (
+    <section className="welcomeArea">
+      <h2>Welcome to Good PR</h2>
+      <p className="welcome-msg">
+        A tracker that helps you visualize the contribution of team members at
+        Code Your Future Final Projects by tracking the number of Pull Requests
+        made.
+      </p>
+    </section>
+  );
+}
+
+export default WelcomeMessage;

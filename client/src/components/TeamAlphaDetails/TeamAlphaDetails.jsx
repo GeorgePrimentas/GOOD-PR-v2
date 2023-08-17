0bf8e2c5f5e1523e641d6f10bdcae8bb85276f93
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2023-08-17 21:19:02 +0100

    create a component call TeamAlphaDetails
    created an object of team member of who created this App information as Data, and how to show the information

---
+++ client/src/components/TeamAlphaDetails/TeamAlphaDetails.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import DeveloperProfile from "../DeveloperProfile/DeveloperProfile";
+
+const teamAlphaData = [
+  {
+    name: "Saghar Hosseinardi",
+    role: "Full-Stack Developer",
+    githubUsername: "SagharHosseinmardi",
+    linkedinUsername: "Saghar Hosseinmardi",
+  },
+  {
+    name: "Andriana Saffo",
+    role: "Full-Stack Developer",
+    githubUsername: "AndrianaOS",
+    linkedinUsername: "Andriana Saffo",
+  },
+  {
+    name: "George Primentas",
+    role: "Front-End Developer",
+    githubUsername: "GeorgePrimentas",
+    linkedinUsername: "George Primentas",
+  },
+  {
+    name: "Gayle Thompson Igwebike",
+    role: "Full-Stack Developer",
+    githubUsername: "Gayle-Thompson-Igwebike",
+    linkedinUsername: "Gayle Thompson-Igwebike",
+  },
+];
+
+const TeamAlphaDetails = () => {
+  return (
+    <div>
+      <h1>Team Alpha</h1>
+      <div className="team-alpha-cards">
+        {teamAlphaData.map((person, index) => (
+          <DeveloperProfile key={index} person={person} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TeamAlphaDetails;

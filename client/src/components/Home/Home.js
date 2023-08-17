4660f5199c0338596e8cf78457a61fea7cee098a
George <108883931+GeorgePrimentas@users.noreply.github.com>
2023-08-17 13:20:00 +0100

    Update Home.js
    Implementation of the HundredSquareChart component to the Home.js component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,12 +5,47 @@ import ClickableCards from "../ClickableCards/ClickableCards";
 import Search from "../Search/Search";
 import TrafficLights from "../TrafficLights/TrafficLights";
 import { Link, useNavigate } from "react-router-dom";
-
+import HundredSquareChart from "../HundredSquareChart/HundredSquareChart";
 function Home() {
   const [teamAndMemberData, setTeamAndMemberData] = useState([]);
   const [teamData, setTeamData] = useState([]);
   const [teamStatuses, setTeamStatuses] = useState([]); // Define the teamStatuses state
   const navigate = useNavigate();
+
+  // const data = ["Meysam-Arshadi", 9, "SanaAsaf", 8, "OlushoreOdude", 8, "ali1373107", 5]
+// const rawData = [{
+//   "id": 31,
+//   "teamName": "NYF",
+//   "owner": "OlushoreOdude",
+//   "repo": "nyf-job-board",
+//   "pullRequestCount": 30,
+//   "users": {
+//     "Meysam-Arshadi": 9,
+//     "SanaAsaf": 8,
+//     "OlushoreOdude": 8,
+//     "ali1373107": 5
+//   }
+
+  const rawData = [{
+  "id": 31,
+  "teamName": "NYF",
+  "owner": "OlushoreOdude",
+  "repo": "nyf-job-board",
+  "pullRequestCount": 30,
+  "users": {
+    "Meysam-Arshadi": 9,
+    "SanaAsaf": 8,
+    "OlushoreOdude": 8,
+    "ali1373107": 5
+  }
+
+}];
+
+const data = [];
+const users = rawData[0].users;
+for (const user in users) {
+  data.push(user, users[user]);
+}
 
   const handleClick = (id) => {
     // Scroll to the top of the page when the link is clicked
@@ -22,7 +57,8 @@ function Home() {
   async function getAllTeamsAndMembersData() {
     try {
       const response = await fetch(
-        `${process.env.REACT_APP_BACKEND_URL}/teamPr`
+        // `${process.env.REACT_APP_BACKEND_URL}/teamPr`
+       `http://localhost:8000/teamPr`
       ); //Change to render site before merging
       const data = await response.json();
       setTeamAndMemberData(data);
@@ -32,7 +68,8 @@ function Home() {
   }
 
   function getAllTeamData() {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/team`)
+    // fetch(`${process.env.REACT_APP_BACKEND_URL}/team`)
+        fetch(`http://localhost:8000/team`)
       .then((response) => response.json())
       .then((data) => {
         setTeamData(data);
@@ -62,6 +99,7 @@ function Home() {
                 teamId={eachTeam.id}
                 teamStatuses={teamStatuses} // Pass teamStatuses to ClickableCards
               />
+
             </Link>
           ))}
       </section>
@@ -71,6 +109,7 @@ function Home() {
       />{" "}
      
       <FormLink />
+      <HundredSquareChart data={data}/>
       <Search />
     </div>
   );

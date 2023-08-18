a89275d6dea345c2e6816edecf07d75716e2165e
Andriana <andrianasaffo@gmail.com>
2023-08-18 16:45:07 +0100

    Hardcoded info removed
    Information passed in from fetch function

--- client/src/components/Icons/GitHubIconSvg.jsx
+++ client/src/components/Icons/GitHubIconSvg.jsx
@@ -1,29 +1,15 @@
 import React from "react";
 import GitHubIcon from "../../images/github-mark.svg";
-import "./GitHubIcon.css"
+import "./GitHubIcon.css";
 
-
-const GitHubIconSvg = () => {
-
-    return (
-        <div className="icons-container">
-       < a 
-       className="githubrepo-icon" 
-        href="https://github.com/Gayle-Thompson-Igwebike/GOOD-PR-v1">
-       <img src={GitHubIcon} alt="github icon"  /> 
-       </a>
-       </div>
-    )
-
-}
+const GitHubIconSvg = ({ teamRepo }) => {
+  return (
+    <div className="icons-container">
+      <a className="githubRepo-icon" href={teamRepo}>
+        <img src={GitHubIcon} alt="github icon" />
+      </a>
+    </div>
+  );
+};
 
 export default GitHubIconSvg;
-
-
-
-
-
-
-
-
-
--- client/src/components/Icons/GoogleDocIconSvg.jsx
+++ client/src/components/Icons/GoogleDocIconSvg.jsx
@@ -1,19 +1,14 @@
 import GoogleDocIcon from "../../images/GoogleDocsLogo.svg";
- import './GoogleDocIcon.css';
+import "./GoogleDocIcon.css";
 
-
-
-const GoogleDocsLogoSvg = () => {
-    return (
-        <div className="icons-container">
-       < a 
-       className="google-doc-project-brief-icon" 
-        href="https://github.com/Gayle-Thompson-Igwebike/GOOD-PR-v1">
-       <img src={GoogleDocIcon} alt="google-doc-project-brief-icon-img"  /> 
-       </a>
-       </div>
-    )
-
-}
+const GoogleDocsLogoSvg = ({ teamBrief }) => {
+  return (
+    <div className="icons-container">
+      <a className="google-doc-project-brief-icon" href={teamBrief}>
+        <img src={GoogleDocIcon} alt="google-doc-project-brief-icon-img" />
+      </a>
+    </div>
+  );
+};
 
 export default GoogleDocsLogoSvg;

c4f19178f0a600e5f0ae35cc4886b438d0534fa0
Andriana <andrianasaffo@gmail.com>
2023-08-18 17:29:15 +0100

    Update Header.jsx
    commented out code removed

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-import logo from '../../images/cyf-logo.png';
-import NavBar from '../NavBar/NavBar.jsx';
-import './Header.css';
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from "../../images/cyf-logo.png";
+import NavBar from "../NavBar/NavBar.jsx";
+import "./Header.css";
 
 const Header = ({ title }) => {
-return (
-
-  <header className="App-header">
-    <div className="logo-container">
-      <img className="logo" src={logo} alt="CYF logo" />
-
-  </div>
+  return (
+    <header className="App-header">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="CYF logo" />
+      </div>
       <NavBar />
     </header>
-
-)
-}
+  );
+};
 
 export default Header;
